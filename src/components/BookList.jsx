af506986d07aae2ba7b1b9b27cd522e0ad482ad6
sadia <sadia@techhivebd.com>
2020-04-01 13:23:09 +0600

    initialize layout

---
+++ src/components/BookList.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  state = {}
+  render() {
+    return (
+      <div className='book-list'>
+        <ul>
+          <li>Rasha</li>
+          <li>Ami Topu</li>
+          <li>Ekaras</li>
+        </ul>
+      </div>
+
+    );
+  }
+}
+
+export default BookList;
+
