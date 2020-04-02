81eb38ef9936c60e45593ee3583268e1adcabcc4
sadia <sadia@techhivebd.com>
2020-04-02 10:07:32 +0600

    transform togglebutton to funtional component

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component,useContext } from 'react';
 import {ThemeContext} from '../contexts/ThemeContext';
-class ToggleTheme extends Component {
-  static contextType = ThemeContext
-  render() { 
-    const {toggleTheme} = this.context;
-    return ( 
-      <button onClick = {toggleTheme}>Toggle Theme</button>
-     );
-  }
+// class ToggleTheme extends Component {
+//   static contextType = ThemeContext
+//   render() { 
+//     const {toggleTheme} = this.context;
+//     return ( 
+//       <button onClick = {toggleTheme}>Toggle Theme</button>
+//      );
+//   }
+// }
+
+const ToggleTheme = () => {
+  const {toggleTheme} = useContext(ThemeContext);
+  return (  
+    <button onClick = {toggleTheme}>Toggle Theme</button>
+
+  );
 }
  
 export default ToggleTheme;
+ 
